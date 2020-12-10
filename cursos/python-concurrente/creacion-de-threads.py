7ee9058782b0967b740b27f696ba28c11bf44529
Armando Mariscal <armando.marisscal@gmail.com>
2020-12-09 23:08:28 -0700

    Creación de threads

---
+++ cursos/python-concurrente/creacion-de-threads.py
@@ -0,0 +1,15 @@
+import requests
+import threading
+
+def get_name():
+    response = requests.get('https://randomuser.me/api/')
+    if response.status_code == 200: #OK
+        results = response.json().get('results')
+        name = results[0].get('name').get('first')
+        print(name)
+
+if __name__ == '__main__':
+    for _ in range(0, 20):
+        # get_name()
+        thread = threading.Thread(target=get_name)
+        thread.start()
